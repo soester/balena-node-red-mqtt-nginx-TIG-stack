ea0779292952d0defc785efb00e3159e0eb69036
Samuel Oester <samuel.oester@messtechnik.ch>
2019-02-10 00:47:12 +0100

    add yoctopuce container

---
+++ yoctopuce/startup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+hubversion=34369
+
+image_and_tag="virtualhub:$hubversion"
+
+if [ -z $(docker images -q ${image_and_tag}) ]
+then
+  echo "${image_and_tag} does not exist, builing image..."
+#  docker build --build-arg virtualhubversion=${hubversion} -t ${image_and_tag} Dockerfile
+  docker build --build-arg virtualhubversion=${hubversion} -t ${image_and_tag} https://raw.githubusercontent.com/scurth/yoctopuce-docker/master/Dockerfile
+fi
+
+docker run --memory 16m -d -p 4444:4444 -ti --restart unless-stopped --privileged -v /dev/bus/usb:/dev/bus/usb ${image_and_tag} /usr/src/yocto/${virtualhub}/armhf/VirtualHub
