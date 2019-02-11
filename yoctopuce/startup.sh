497fccf2d566fafc7ac72ad54b65117468bb8091
Samuel Oester <samuel.oester@messtechnik.ch>
2019-02-12 00:11:33 +0100

    yoctopuce/Dockerfile

--- yoctopuce/startup.sh
+++ yoctopuce/startup.sh
@@ -7,7 +7,7 @@ if [ -z $(docker images -q ${image_and_tag}) ]
 then
   echo "${image_and_tag} does not exist, builing image..."
 #  docker build --build-arg virtualhubversion=${hubversion} -t ${image_and_tag} Dockerfile
-  docker build --build-arg virtualhubversion=${hubversion} -t ${image_and_tag} Dockerfile
+  docker build --build-arg virtualhubversion=${hubversion} -t ${image_and_tag} yoctopuce/Dockerfile
 fi
 
 docker run -d --name virtualhub -p 4444:4444 --privileged -v /dev/bus/usb:/dev/bus/usb -u 997 --restart=always ${image_and_tag} /usr/src/yocto/${virtualhub}/armhf/VirtualHub
