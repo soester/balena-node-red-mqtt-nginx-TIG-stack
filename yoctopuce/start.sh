3eaa21e2000d29b3cbd6f33af124ff160340f46d
soester <sam@IMACWSC.local>
2019-02-25 14:47:51 +0100

    spaces

--- yoctopuce/start.sh
+++ yoctopuce/start.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
-
 # Maybe needed to take into account the new yvirtualhub.service
 systemctl daemon-reload
-
 # Enable yvirtualhub service boot loading
 systemctl enable yvirtualhub
-
 # Start yvirtualhub service
 systemctl start yvirtualhub
-
 # To prevent Docker from exiting
 #journalctl -f
